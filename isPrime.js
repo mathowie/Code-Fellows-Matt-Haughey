7be384883d1b016a0382cb6e2868e6be25dc5fa7
mathowie <matt@haughey.com>
2014-11-10 21:11:50 -0800

    add day three stuff

---
+++ isPrime.js
@@ -0,0 +1,6 @@
+var prime
+
+for (var i = 2; i <= prime; i++) {
+  document.write(i);
+
+}
---
+++ prime-numbers.js
@@ -0,0 +1,11 @@
+
+
+//divided only by one or itself
+// whole number greater than 1
+
+var count = 1;
+
+
+
+alert(count);
+
